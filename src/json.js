5be34217eb155c6c45a09fb7a5178d0a25abe5ad
developer-bharat <bharatdeveloperteam@gmail.com>
2022-11-07 15:24:11 +0000

    Changed survey

--- src/json.js
+++ src/json.js
@@ -1,59 +1,34 @@
 export const surveyJson = {
-    "title": "Travel Tripper",
-    "description": "Your personal itinerary creator. ",
     "logoPosition": "right",
     "pages": [
         {
             "name": "page1",
             "elements": [
                 {
-                    "type": "radiogroup",
+                    "type": "text",
                     "name": "question1",
-                    "title": "How often do you travel",
+                    "title": "Enter your name",
                     "isRequired": true,
-                    "choices": [
-                        {
-                            "value": "item1",
-                            "text": "Once a week"
-                        },
-                        {
-                            "value": "item2",
-                            "text": "Once a year"
-                        },
-                        {
-                            "value": "item3",
-                            "text": "I am always on vacation"
-                        },
-                        {
-                            "value": "item4",
-                            "text": "I love my home"
-                        }
-                    ],
-                    "showOtherItem": true
+                    "requiredErrorText": "Please enter your name",
+                    "placeholder": "Jhon Doe"
                 },
                 {
-                    "type": "radiogroup",
+                    "type": "text",
                     "name": "question2",
-                    "title": "What is your favourite destination",
+                    "title": "Enter your email",
                     "isRequired": true,
-                    "choices": [
-                        {
-                            "value": "item1",
-                            "text": "Mountains"
-                        },
-                        {
-                            "value": "item2",
-                            "text": "Sea"
-                        },
-                        {
-                            "value": "item3",
-                            "text": "Forest"
-                        },
-                        {
-                            "value": "item4",
-                            "text": "Adventure sports"
-                        }
-                    ]
+                    "requiredErrorText": "Please enter your email",
+                    "inputType": "email",
+                    "autocomplete": "email",
+                    "placeholder": "jhon.doe@gmail.com"
+                },
+                {
+                    "type": "comment",
+                    "name": "question3",
+                    "title": "Enter your message",
+                    "isRequired": true,
+                    "requiredErrorText": "Please enter your secret message",
+                    "placeholder": "Enter your secret message"
                 }
             ]
         }
